9db54cdb065d83318bf895dadb44a57a9f8d145a
Manuel Fissore <manuel.fissore.37@gmail.com>
2023-03-28 10:31:01 +0200

    es2 works

--- lab1/2/project/main.c
+++ lab1/2/project/main.c
@@ -2,95 +2,84 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include  <string.h>
 #include <stdlib.h>
 
-#define N 31
-
-typedef struct record_s{
-    int numLine;
-    long int reg;
-    char surname[N];
-    char name[N];
+#define L 31
+typedef struct student_s {
+    int id;
+    long int rn;
+    char n[L];
+    char s[L];
     int mark;
-    struct record_s *next;
-} record_t;
+} student_t;
 
-int main(int argc, char **argv ) {
-    char input_char;
-    int fd, not_end = 1;
-    int input_int = 0;
-    record_t *r;
-    record_t *tmp;
-    record_t *head;
-    int i = 0, found = 1, n = 0;
-    head = NULL;
-    tmp = (record_t *) malloc(1 * sizeof(record_t));
-    fd = open(argv[1], O_RDONLY);
-    do {
 
-        not_end = read(fd, &tmp, sizeof(record_t));
-        if (not_end) {
-            r=(record_t *) malloc(1 * sizeof(record_t));
-            r=tmp;
-            r->next = head;
-            head = r;
-            n++;
-        }
-    } while (not_end);
+int main (int argc, char **argv) {
+    char str[L], command[L];
+    int fd, n;
+    student_t student_d;
 
-    while (1) {
-        fscanf(stdin, "%c %d", &input_char, &input_int);
-        switch (input_char) {
-            case 'R':
-                found = 0;
-                tmp = head;
-                i = 0;
-                while (!found) {
-                    if (tmp->numLine == input_int) {
-                        printf("%d %ld %s %s %d\n", r->numLine, r->reg, r->surname, r->name, r->mark);
-                    } else {
-                        tmp = tmp->next;
-                        if (i++ == n) {
-                            printf("Not found\n");
-                            found = 1;
-                        }
-
-                    }
-                }
-                break;
-            case 'W':
-                found = 0;
-                i = 0;
-                tmp = head;
-                record_t *t;
-                printf("Data: ");
-                t = (record_t *) malloc(1 * sizeof(record_t));
-                fscanf(stdin, "%d %ld %s %s %d", &t->numLine, &t->reg, t->surname, t->name, &t->mark);
-                if (t->numLine<n){
-                    for (i=0; i<t->numLine;i++)
-                        tmp=tmp->next;
-                }
-                else {
-                    for (i = 0; i < t->numLine; i++) {
-                        if (i < n)
-                            tmp = tmp->next;
-                        else {
-                            tmp->next = (record_t *) malloc(1 * sizeof(record_t));
-                            tmp = tmp->next;
-                            tmp->numLine=-1;
-                        }
-                    }
-                }
-                tmp=t;
-                break;
-            case 'E':
-                return 0;
-                break;
-            default:
-                printf("Input not valid\n");
-                break;
-        }
+    if (argc != 2) {
+        fprintf (stderr, "Usage: pgrm file-binary\n");
+        exit (1);
     }
-}
 
+    /*
+     *  Final Check: File OK?
+     */
+
+    fprintf (stdout,  "Final file:\n");
+    fd = open (argv[1], O_RDWR);
+    if (fd == -1){
+        fprintf (stderr, "Error opening input file (%s).\n", argv[1]);
+        exit (1);
+    }
+
+    while ((n = read (fd, &student_d, sizeof (student_t))) ==
+           sizeof (student_d)) {
+        fprintf (stdout, "%d %ld %s %s %d\n", student_d.id, student_d.rn,
+                 student_d.n, student_d.s, student_d.mark);
+    }
+
+    /*
+     *  Core
+     */
+
+    do {
+        fprintf (stdout,  "Command: ");
+        fgets (str, L, stdin);
+        sscanf (str, "%s", command);
+        if (strcmp (command, "E")==0) {
+            break;
+        }
+        sscanf (str, "%*s%d", &n);
+        lseek(fd, ((n-1)*sizeof(student_t)), SEEK_SET);
+        if (strcmp (command, "R")==0) {
+            read (fd, &student_d, sizeof (student_t));
+            fprintf (stdout, "%d %ld %s %s% d\n", student_d.id, student_d.rn,
+                     student_d.n, student_d.s, student_d.mark);
+        } else if (strcmp (command, "W")==0) {
+            scanf ("%d%ld%s%s%d", &student_d.id, &student_d.rn,
+                   student_d.n, student_d.s, &student_d.mark);
+            write (fd, &student_d, sizeof (student_t));
+        }
+    } while (strcmp(command, "E") != 0);
+
+    /*
+     *  Final Check: File OK?
+     */
+
+    fprintf (stdout,  "Final file:\n");
+    lseek (fd, 0, SEEK_SET);
+    while ((n = read (fd, &student_d, sizeof (student_t))) ==
+           sizeof (student_d)) {
+        fprintf (stdout, "%d %ld %s %s %d\n", student_d.id, student_d.rn,
+                 student_d.n, student_d.s, student_d.mark);
+    }
+
+    close (fd);
+
+    return 0;
+}
 
