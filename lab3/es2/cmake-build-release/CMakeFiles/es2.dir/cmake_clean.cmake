5951e2f6e1455fac227025aeded459222313e644
Manuel Fissore <manuel.fissore.37@gmail.com>
2023-04-23 11:22:58 +0200

    lab3 es1 almost done

---
+++ lab3/es2/cmake-build-release/CMakeFiles/es2.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/es2.dir/main.cpp.o"
+  "CMakeFiles/es2.dir/main.cpp.o.d"
+  "es2"
+  "es2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/es2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- lab3/es2/main.cpp
+++ lab3/es2/main.cpp
@@ -1,55 +1,55 @@
 #include <iostream>
 #include <iostream>
-#include <map>
 #include <set>
-#include <vector>
-
+#include <string>
 using namespace std;
-typedef struct player{
-    string name;
+
+struct Player {
+    std::string name;
     int score;
-}Player;
-
-
-template <typename T>
-Player cmp (const T& v1, const T& v2){
-    return((v1>v2)?true:false);
-}
-
-
-class LeaderBord {
-private:
-    set <Player, > board;
-    public:
-        void addPlayer(const std::string& name, int score){
-            Player p;
-            p.name=name;
-            p.score=score;
-            board.insert(board.begin(), (Player) p);
-        };
-        void removePlayer(const std::string& name){
-            for(auto it: board){
-                if (it.name==name)
-                    board.erase(it);
-            }
-        };
-        void updateScore(const std::string& name, int newScore){
-            for(auto it: board){
-                if (it.name==name)
-                    it.score=newScore;
-            }
-        };
-        void printTopPlayers(int n){
-
-        };
 };
 
+bool operator<(const Player& p1, const Player& p2) {
+    return p1.score > p2.score || (p1.score == p2.score && p1.name < p2.name);
+}
+
+class Leaderboard {
+public:
+    void addPlayer(const std::string& name, int score) {
+        players_.insert({name, score});
+    }
+
+    void removePlayer(const std::string& name) {
+        auto it = std::find_if(players_.begin(), players_.end(),
+                               [&name](const auto& player) { return player.name == name; });
+        if (it != players_.end()) {
+            players_.erase(it);
+        }
+    }
+
+    void updateScore(const std::string& name, int newScore) {
+        removePlayer(name);
+        addPlayer(name, newScore);
+    }
+
+    void printTopPlayers(int n) const {
+        int i = 0;
+        for (const auto& player : players_) {
+            if (i >= n) break;
+            std::cout << player.name << ": " << player.score << '\n';
+            ++i;
+        }
+    }
+
+private:
+    std::set<Player> players_;
+};
 
 int main() {
-    LeaderBord leaderBord;
+    Leaderboard leaderboard;
     int v, n;
     int flag=1;
-    player p;
+    Player p;
     while (cin>>v && flag){
         switch(v){
             case 1:
@@ -57,27 +57,27 @@ int main() {
                 cin>>p.name;
                 cout<<"aggiungi score"<<endl;
                 cin>>p.score;
-                leaderBord.addPlayer(p.name, p.score);
+                leaderboard.addPlayer(p.name, p.score);
                 break;
             case 2:
                 cout<<"nome del giocatore da eliminare"<<endl;
                 cin>>p.name;
-                leaderBord.removePlayer(p.name);
+                leaderboard.removePlayer(p.name);
                 break;
             case 3:
                 cout<<"nome del giocatore da modificare"<<endl;
                 cin>>p.name;
                 cout<<"nuovo score"<<endl;
                 cin>>p.score;
-                leaderBord.updateScore(p.name, p.score);
+                leaderboard.updateScore(p.name, p.score);
                 break;
             case 4:
                 cout<<"quante posizioni vuoi vedere?"<<endl;
                 cin>>n;
-                leaderBord.printTopPlayers(n);
+                leaderboard.printTopPlayers(n);
                 break;
             default:
-                flag=!flag;
+                flag=(!flag);
                 break;
         }
     }
